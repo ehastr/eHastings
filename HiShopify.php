3a0e3a1df0e065b8828767efdbe6478390d01c5e
Erica Hastings <ehastr@gmail.com>
2015-11-10 00:12:37 -0500

    Renamed my letter to Shopify.

---
+++ HiShopify.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta property="og:title" content="Erica Hastings" />
+    <meta property="og:description" content="Erica Hastings&#39;s online resumé" />
+    <meta property="og:image" content="https://ericahastings.com/assets/images/opengraph.jpg" />
+    <meta property="og:url" content="https://ericahastings.com" />
+    <meta name="description" content="Erica Hastings&#39;s online resumé." />
+    <meta name="keywords" content="Erica Hastings" />
+    <meta name="author" content="Erica Hastings" />
+    <title>Erica Hastings</title>
+    <link href="/assets/styles/style.css" rel="stylesheet" type="text/css" /><? if (isset($_GET['print'])) { ?>
+    <link href="/assets/styles/style.print.css" rel="stylesheet" type="text/css" /><?php } ?>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/assets/scripts/jquery.viewport.mini.js"></script>
+    <script type="text/javascript" src="/assets/scripts/custom.js"></script>
+    <!--[if lte IE 8]>
+        <script type="text/javascript" src="assets/scripts/modernizr.custom.js"></script>       
+        <link href="assets/styles/style.ie8.css" rel="stylesheet" type="text/css">
+    <![endif]-->
+  </head>
+  <body>
+    <aside>
+      <header>
+      <img id="qrcode" class="print" src="/assets/images/qr.gif" alt="ericahastings.com QR code" />
+      <span>Erica</span> 
+      <span>Hastings</span></header>
+      <ul class="sidebar_list">
+        <li>
+        <span class="gray">email |</span> 
+        <a href="mailto:ehastr@gmail.com">ehastr@gmail.com</a></li>
+        <li>
+        <span class="gray">phone |</span> 
+        <a href="tel:+16138640164">+1 (613) 263 3037</a></li>
+      </ul>
+      <ul>
+        <li class="noprint">
+        <span class="gray">follow |</span> 
+        <a href="https://facebook.com/erica.hastings">
+          <span>facebook</span>
+        </a></li>
+      </ul>
+      <ul id="sidebar_list_view">
+        <li class="noprint">
+          <a href="/?print">print-friendly</a>
+        </li>
+      </ul>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    <article id="content">
+    <section id="shopify">
+      <h1>Hi Shopify!</h1>
+		<p>
+			<em>Ottawa, ON<br/>
+			C/O: Talent Acquisition<br/>
+ 			<br/>
+			RE: Technical Compliance Analyst Position<br/></em>
+		</p>
+			<br/>
+			<br/>
+		<p>
+			<strong>To whom it may concern:</strong>
+		</p>
+			<br/>
+		<p>
+			I have finally found my chance to join Shopify. Let me be clear: I’m using the word ‘finally’ in its classical sense. For years, during co-op season at the University of Ottawa, I have heard the almost magical pitch about how wonderful the company is. For years, I would ask, “Got anything for an accounting and marketing student yet?” And for years, your recruiters would say, “Right now, we’re primarily interested in computer science majors. But – hey, never say never!” Then my CS major friends would squeal across Facebook saying yes, Shopify is excellent, and yes, it was too bad I had to study for a Bachelor of Commerce and intern at Aboriginal Affairs and Northern Development Canada’s Audit and Assurance Services department instead.
+		</p>
+			<br/>
+		<p>
+			I’ve never been picked last for a team, largely because I spend so much of my time writing sensitive reports and documenting compliance issues across the federal government, but I imagine that’s what it feels like.
+		</p>
+			<br/>
+		<p>
+			Today, however, I am applying to your Technical Compliance Analyst position. After several successful years working as an auditor for the Department of National Defence, the skills I have to bring your Security Operations Team include hefty testing of technical and financial controls, explaining conclusions on compliance data to clients, breaking down complicated analysis and observations for stakeholders, re-explaining that breakdown to my audit peers, and above all, experience in an auditor-facing role. You are finally – <em>finally</em> – interested in someone like me.
+		</p>
+			<br/>
+		<p>
+			I’m able speak with clients at every level, adjusting for whether they’re top-level Comptrollers or unfamiliar with management information systems. For the entirety of my professional career, I’ve explained how particular controls should be implemented, communicated potential concerns or inefficiencies in support of my organization’s standards, and publicly published my team’s findings with lengthy trails of audit evidence to support each claim. I’m not only enthusiastic about these responsibilities, but polite, friendly, and approachable about them.
+		</p>
+			<br/>
+		<p>
+			Within a team environment, I excel when given a deadline. It allows me to prioritize my resources and communicate with my colleagues to connect smaller tasks into a greater picture. I also work very well on independent projects, particularly once the expectations of results are made clear. I ask questions to verify my understanding and solicit feedback for further improvement, but I’ll frequently investigate topics on my own first.
+		</p>
+			<br/>
+		<p>
+			Finally, I am highly practiced across the Microsoft suite and able to pick up the rules of a new system quickly. Adding on to this, I’m able to translate what I know to newcomers and make sure each interested party is on the same page.
+		</p>
+			<br/>
+		<p>
+			Thank you for taking the time to consider my application. If you have any questions or comments, please feel free to contact me. Until then, I have some Facebook friends to reply to.
+		</p>
+			<br/>
+		<p>
+			Best regards,<br/>
+			<strong>Erica Hastings</strong>
+		</p>
+    </section></article>
+  </body>
+</html>
